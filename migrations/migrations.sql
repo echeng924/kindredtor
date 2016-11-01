94ae78fb6f974e16d8adb836b000b7e32cd5d356
Elizabeth Cheng <echeng924@gmail.com>
2016-11-01 12:21:07 -0400

    setup for migrations file

---
+++ migrations/migrations.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE networking
+
+\c networking
+
+CREATE TABLE members (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password VARCHAR NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  location VARCHAR NOT NULL,
+  current_title VARCHAR NOT NULL,
+  role VARCHAR NOT NULL,
+  current_industry VARCHAR,
+  interested_tech VARCHAR NOT NULL,
+  blurb TEXT NOT NULL,
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  member_id VARCHAR NOT NULL,
+);
